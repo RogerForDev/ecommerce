7739bc05725b21a5e69264fd06c3bc4b78af1b68
Roger William <rogmtsi.df@gmail.com>
2018-07-16 12:42:38 -0300

    crud categorias

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -199,6 +200,67 @@ $app->post("/admin/forgot/reset", function(){
 	]);
 
 	$page->setTpl("forgot-reset-success");
+
+});
+
+$app->get('/admin/categories', function(){
+	
+	$page = new PageAdmin();
+
+	$categories = Category::listAll();
+	
+	$page->setTpl("categories", ["categories"=>$categories]);
+});
+
+$app->get('/admin/categories/create', function(){
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+});
+
+$app->get('/admin/categories/:idcategory', function($idcategory){
+	$page = new PageAdmin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page->setTpl("categories-update", ["category"=>$category->getValues()]);
+});
+
+$app->post('/admin/categories/:idcategory', function($idcategory){
+	$page = new PageAdmin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page->setTpl("categories-update", ["category"=>$category->getValues()]);
+});
+
+$app->post('/admin/categories/create', function(){
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
 
 });
 $app->run();
