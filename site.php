d63c1eb23ee9cff5b2a994edcf6f5f628f2ee3c2
Roger William <rogmtsi.df@gmail.com>
2018-07-20 14:04:50 -0300

    Carrinho calculo frete

--- site.php
+++ site.php
@@ -66,7 +66,8 @@ $app->get('/cart', function(){
 	$page = new Page();
 	$page->setTpl('cart', [
 		"cart"=>$cart->getValues(),
-		"products"=>$cart->getProducts()
+		"products"=>$cart->getProducts(),
+		"error"=>Cart::getMsgError()
 	]);
 
 });
@@ -117,6 +118,15 @@ $app->get('/cart/:idproduct/remove', function($idproduct){
 	header("Location: /cart");
 	exit;
 
+});
+
+$app->post('/cart/freight', function(){
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
+
+	header("Location: /cart");
+	exit;
 });
 
 
