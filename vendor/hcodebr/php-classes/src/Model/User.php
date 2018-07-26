21e4acac984e80265180228d4599a434c468faca
Roger William <rogmtsi.df@gmail.com>
2018-07-26 15:32:59 -0300

    Esqueceu a senha site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -78,7 +78,7 @@ class User extends Model {
 
     }
 
-    public static function getForgot($email){
+    public static function getForgot($email, $inadmin = true){
         $sql = new Sql();
 
         $results = $sql->select("
@@ -106,8 +106,11 @@ class User extends Model {
 
                 base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
 
-                $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
-
+                if ($inadmin === true) {
+                    $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+                } else {
+                    $link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+                }
                 $mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir senha da Hcode Store", "forgot", array(
                     "name"=>$data["desperson"],
                     "link"=>$link
